df3104dd45f0ee665a8253db114552a6d7c81027
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2020-03-09 16:35:09 +0200

    [RFC] Tools: Test: Cleanup component run scripts
    The src_run.sh is converted into a generic component run script
    that is called by new simple component specific scripts asrc_run.sh,
    eqfir_run.sh, eqiir_run.sh, src_run.sh, and volume_run.sh.
    
    Note: The testbench does not yet have fully working capture support
    so the direction is not exposed yet.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- tools/test/audio/asrc_run.sh
+++ tools/test/audio/asrc_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright(c) 2018-2020 Intel Corporation. All rights reserved.
+# Copyright(c) 2020 Intel Corporation. All rights reserved.
 
 if [ -z "$6" ]; then
     echo "Usage:   $0 <bits in> <bits out> <rate in> <rate out> <input> <output>"
